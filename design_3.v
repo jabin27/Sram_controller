c3607b85ef99effa7a096d2c6416ee44b4e86b5d
Jabin Sultana <65303168+jabin27@users.noreply.github.com>
2024-05-09 11:20:20 +0600

    Update design_3.v

--- design_3.v
+++ design_3.v
@@ -40,110 +40,115 @@ always @(posedge clk , posedge reset)
     addr_reg <= 0;
     data_f2s_reg <= 0; 
     data_s2f_reg <= 0;
-tri_reg <= 1'b1;
-we_temp_reg <= 1'b1;
-oe_reg <= 1'b1; 
-end 
-else
-begin 
-state_reg <= state_next ; 
-addr_reg <= addr_next ; 
-data_f2s_reg <= data_f2s_next ; 
-data_s2f_reg <= data_s2f_next ; 
-tri_reg <= tri_buf;
-we_temp_reg <= we_temp_buf;
-oe_reg <= oe_buf; 
-end 
+    tri_reg <= 1'b1;
+    we_temp_reg <= 1'b1;
+    oe_reg <= 1'b1; 
+  end 
+  else
+  begin 
+    state_reg <= state_next ; 
+    addr_reg <= addr_next ; 
+    data_f2s_reg <= data_f2s_next ; 
+    data_s2f_reg <= data_s2f_next ; 
+    tri_reg <= tri_buf;
+    we_temp_reg <= we_temp_buf;
+    oe_reg <= oe_buf; 
+  end 
+ 
 // FSMD next-state logic 
 always @* 
 begin 
-addr_next = addr_reg ; 
-data_f2s_next = data_f2s_reg; 
-data_s2f_next = data_s2f_reg; 
-ready = 1'b0; 
-case (state_reg)
-idle : 
-begin
-if (~mem)
-state_next = idle; 
-else
-begin 
-addr_next = addr; 
-if (~rw) // write 
-begin 
-state_next = wr1;
-data_f2s_next = data_f2s ; 
-end 
-else // read 
-state_next = rd1; 
-end 
-ready = 1'b1; 
-end 
-wr1: 
-begin
-if (~mem)
-state_next = idle; 
-else
-begin 
-addr_next = addr; 
-if (~rw) // write 
-begin 
-state_next = wr1;
-data_f2s_next = data_f2s ; 
-end 
-else // read 
-state_next = rd1; 
-end 
-ready = 1'b1; 
-end 
+  addr_next = addr_reg ; 
+  data_f2s_next = data_f2s_reg; 
+  data_s2f_next = data_s2f_reg; 
+  ready = 1'b0; 
+  case (state_reg)
+  idle : 
+  begin
+    if (~mem)
+        state_next = idle; 
+    else
+    begin 
+    addr_next = addr; 
+      if (~rw) // write 
+      begin 
+        state_next = wr1;
+        data_f2s_next = data_f2s ; 
+      end 
+      else // read 
+        state_next = rd1; 
+    end 
+    ready = 1'b1; 
+  end 
+  wr1: 
+  begin
+    if (~mem)
+      state_next = idle; 
+    else
+    begin 
+      addr_next = addr; 
+    if (~rw) // write 
+    begin 
+      state_next = wr1;
+      data_f2s_next = data_f2s ; 
+    end 
+    else // read 
+      state_next = rd1; 
+    end 
+      ready = 1'b1; 
+  end 
   
-rd1: 
-begin 
-data_s2f_next = dio_a;
-if (~mem)
-state_next = idle; 
-else
-begin 
-addr_next = addr; 
-if (~rw) // write 
-begin 
-state_next = wr1;
-data_f2s_next = data_f2s ; 
-end 
-else // read 
-state_next = rd1; 
-end 
-ready = 1'b1; 
-end   
-default : 
-state_next = idle; 
+  rd1: 
+  begin 
+    data_s2f_next = dio_a;
+    if (~mem)
+      state_next = idle; 
+    else
+    begin 
+      addr_next = addr; 
+      if (~rw) // write 
+      begin 
+       state_next = wr1;
+       data_f2s_next = data_f2s ; 
+      end 
+      else // read 
+        state_next = rd1; 
+    end 
+    ready = 1'b1; 
+   end   
+   default : 
+      state_next = idle; 
 endcase 
 end 
+ 
 // look-ahead output logic 
 always @* 
 begin 
-tri_buf = 1'b1; // signals are active low
-we_temp_buf = 1'b1;
-oe_buf = 1'b1; 
-case (state_next)
-idle : 
-oe_buf = 1'b1; 
-wr1: 
-begin 
-tri_buf = 1'b0;
-we_temp_buf = 1'b0; 
-end  
-rd1: 
-oe_buf = 1'b0;  
-endcase 
+  tri_buf = 1'b1; // signals are active low
+  we_temp_buf = 1'b1;
+  oe_buf = 1'b1; 
+  case (state_next)
+  idle : 
+    oe_buf = 1'b1; 
+  wr1: 
+  begin 
+    tri_buf = 1'b0;
+    we_temp_buf = 1'b0; 
+  end  
+  rd1: 
+    oe_buf = 1'b0;  
+  endcase 
 end 
+ 
 // to main system 
 assign data_s2f_r = data_s2f_reg; 
 assign data_s2f_ur = dio_a; 
+ 
 // to sram 
 assign we_n = we_temp_reg | ~clk; 
 assign oe_n = oe_reg; 
 assign ad = addr_reg; 
+ 
 // i/o for sram chip a 
 assign ce_a_n = 1'b0; 
 assign ub_a_n = 1'b0; 
